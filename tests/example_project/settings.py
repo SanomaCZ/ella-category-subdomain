8e62f5e8fbdd8448dffe76afbec5cfe44e020fcf
Tomas Flam <flam@sanoma.cz>
2011-11-24 16:05:29 +0100

    settings moved to one file

--- tests/example_project/settings.py
+++ tests/example_project/settings.py
@@ -1,36 +1,40 @@
 from os.path import join, dirname, abspath
+from tempfile import gettempdir
 
 import example_project
 
-PROJECT_ROOT = dirname(dirname(abspath(__file__)))
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+SHOW_DEBUG_TOOLBAR = DEBUG
+DEVELOPMENT_SERVER_PORT = 8000
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': join(gettempdir(), 'example_project.db'), # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+SECRET_KEY = '1#h7o0k$ea%ciox$$p&@(r&1eokb*mvk(n(v9!wb11bt(^4$ns'
+
+PROJECT_ROOT = dirname(abspath(__file__))
+
 TIME_ZONE = 'Europe/Prague'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = join(PROJECT_ROOT, 'static')
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = '/static/'
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
@@ -84,3 +88,19 @@ NEWMAN_MEDIA_PREFIX = MEDIA_URL + 'newman/'
 # ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin/'
 
 DEFAULT_MARKUP = 'markdown'
+
+CACHE_BACKEND = 'dummy://'
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
