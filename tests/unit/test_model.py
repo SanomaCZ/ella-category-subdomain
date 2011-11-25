c4639a732e0121f3fc0891ed13c247a27423b580
Jakub Skaryd <skaryd@sanoma.cz>
2011-11-25 11:46:41 +0100

    Model tests

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,56 +1,34 @@
 from django.contrib.sites.models import Site
-
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from djangosanetesting import DatabaseTestCase
 
 from ella.core.models import Category
-
 from ella_category_subdomain.models import CategorySubdomain
 
-class TestModel(DatabaseTestCase):
+from helpers import create_categories
+
+class TestModelCase(DatabaseTestCase):
 
     def setUp(self):
         super(TestModel, self).setUp()
-
-        site = Site.objects.create(
-            domain=u'example.com',
-            name=u'main domain',
-        )
-
-        self.root = Category.objects.create(
-            title=u'Main',
-            slug=u'main',
-            description=u'Main Category',
-            site=site,
-        )
-
-        self.first_level_1 = Category.objects.create(
-            title=u'First 1',
-            slug=u'first_1',
-            description=u'First Category',
-            site=site,
-            tree_parent=self.root,
-        )
-
-        self.first_level_2 = Category.objects.create(
-            title=u'First 2',
-            slug=u'first_2',
-            description=u'First Category',
-            site=site,
-            tree_parent=self.root,
-        )
-
-        self.second_level_2 = Category.objects.create(
-            title=u'Second 1',
-            slug=u'second_1',
-            description=u'Second Category',
-            site=site,
-            tree_parent=self.first_level_1,
-        )
-
-    def test_equals(self):
-        self.assert_equals(1,1)
+        create_categories(self)
 
     def test_root_category(self):
-        cs = CategorySubdomain(category = self.root, subdomain_slug = "root")
-        self.assert_va
+        cs = CategorySubdomain(category = self.root_category, subdomain_slug = self.root_category.slug)
+        self.assert_raises(ValidationError, cs.clean)
+
+    def test_first_nested_category(self):
+        cs = CategorySubdomain(category = self.category_nested_1, subdomain_slug = self.category_nested_1.slug)
+        self.assert_is_none(cs.clean())
+
+    def test_second_nested_category(self):
+        cs = CategorySubdomain(category = self.category_nested_nested_2, subdomain_slug = self.category_nested_nested_2.slug)
+        self.assert_raises(ValidationError, cs.clean)
+
+    def test_single_reference_to_category(self):
+        cs1 = CategorySubdomain(category = self.category_nested_1, subdomain_slug = self.category_nested_1.slug)
+        cs1.save()
+        cs2 = CategorySubdomain(category = self.category_nested_1, subdomain_slug = self.category_nested_1.slug)
+        self.assert_raises(IntegrityError, cs2.save)
 
