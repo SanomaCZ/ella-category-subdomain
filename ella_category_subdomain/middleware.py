b3658c1350096a9581d6dbb3119105c2fb8e4b0a
Tomas Flam <flam@sanoma.cz>
2011-11-28 11:04:28 +0100

    changed middleware and monkeypatch

--- ella_category_subdomain/middleware.py
+++ ella_category_subdomain/middleware.py
@@ -9,23 +9,17 @@ from django.conf import settings
 from .models import CategorySubdomain
 from .monkeypatch import do_monkeypatch
 
+
 class CategorySubdomainMiddleware:
 
     def __init__(self):
         do_monkeypatch()
 
     def process_request(self, request):
-        if request.path_info.startswith(settings.MEDIA_URL):
-            return None
-        sc = self._get_subdomain_category(request.get_host())
-        if sc is not None:
-            request.path_info = '/%s%s' % (sc.slug, request.path_info)
-            request.path = '/%s%s' % (sc.slug, request.path)
         return None
 
     def process_response(self, request, response):
         return response
-
 
     def _get_subdomain_category(self, host):
         domain_parts = host.split('.')
--- ella_category_subdomain/monkeypatch.py
+++ ella_category_subdomain/monkeypatch.py
@@ -1,91 +1,27 @@
+from urlparse import urlparse
+
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import (reverse, RegexURLPattern,
+                                      RegexURLResolver)
 from django.contrib.sites.models import Site
 from django.template.defaultfilters import slugify
+import django.conf.urls.defaults
 
 from .models import CategorySubdomain
 from ella.core.models import Category, Placement
 from ella.core.cache import get_cached_object
 
 
+original_url = django.conf.urls.defaults.url
+
+def url(regex, view, kwargs=None, name=None, prefix=''):
+    regex_url = original_url(regex, view, kwargs, name, prefix)
+    if isinstance(regex_url, RegexURLPattern):
+        pass
+    if isinstance(regex_url, RegexURLResolver):
+        pass
+    return regex_url
+
+
 def do_monkeypatch():
-
-    def category_get_absolute_url(category):
-        # taken from ella.core.models.Category except try/except statement
-        if not category.tree_parent_id:
-            url = reverse('root_homepage')
-        else:
-            url = reverse(
-                    'category_detail',
-                    kwargs={
-                        'category' : category.tree_path,
-                    }
-                )
-        site = get_cached_object(Site, pk=category.site_id)
-        if category.site_id != settings.SITE_ID:
-            # prepend the domain if it doesn't match current Site
-            return 'http://' + site.domain + url
-        try:
-            cs = category.categorysubdomain
-        except CategorySubdomain.DoesNotExist:
-            cs = None
-        if settings.DEBUG and hasattr(settings, 'DEVELOPMENT_SERVER_PORT'):
-            port = ':%s' % settings.DEVELOPMENT_SERVER_PORT
-        else:
-            port = ''
-        if cs is None:
-            absolute_url = 'http://%s%s/' % (site.domain, port)
-        else:
-            absolute_url = cs.get_absolute_url()
-        url = url.lstrip('/' + category.slug)
-        return absolute_url + url.lstrip('/')
-    Category.get_absolute_url = category_get_absolute_url
-
-    def placement_get_absolute_url(placement, domain=False):
-        # taken from ella.core.models.Placement except try/except statement
-        obj = placement.publishable
-        category = placement.category
-
-        kwargs = {
-            'content_type' : slugify(obj.content_type.model_class()._meta.verbose_name_plural),
-            'slug' : placement.slug,
-        }
-
-        if placement.static:
-            if category.tree_parent_id:
-                kwargs['category'] = category.tree_path
-                url = reverse('static_detail', kwargs=kwargs)
-            else:
-                url = reverse('home_static_detail', kwargs=kwargs)
-        else:
-            kwargs.update({
-                    'year' : placement.publish_from.year,
-                    'month' : placement.publish_from.month,
-                    'day' : placement.publish_from.day,
-                })
-            if category.tree_parent_id:
-                kwargs['category'] = category.tree_path
-                url = reverse('object_detail', kwargs=kwargs)
-            else:
-                url = reverse('home_object_detail', kwargs=kwargs)
-
-        site = get_cached_object(Site, pk=category.site_id)
-        try:
-            cs = category.categorysubdomain
-        except CategorySubdomain.DoesNotExist:
-            cs = None
-        if settings.DEBUG and hasattr(settings, 'DEVELOPMENT_SERVER_PORT'):
-            port = ':%s' % settings.DEVELOPMENT_SERVER_PORT
-        else:
-            port = ''
-        if cs is None:
-            absolute_url = 'http://%s%s/' % (site.domain, port)
-        else:
-            absolute_url = cs.get_absolute_url()
-        url = url.lstrip('/' + category.slug)
-        return absolute_url + url.lstrip('/')
-
-        if category.site_id != settings.SITE_ID or domain:
-            return 'http://' + site.domain + url
-        return url
-    Placement.get_absolute_url = placement_get_absolute_url
+    django.conf.urls.defaults.url = url
