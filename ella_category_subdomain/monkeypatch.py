703b50d79d7242486f7292206993ce8f6feff4d1
Tomas Flam <flam@sanoma.cz>
2011-11-29 11:29:12 +0100

    added comments

--- ella_category_subdomain/monkeypatch.py
+++ ella_category_subdomain/monkeypatch.py
@@ -3,66 +3,106 @@ from urlparse import urlparse, urlunparse
 import logging
 
 from django.conf import settings
+from django.contrib.sites.models import Site
 import django.conf.urls.defaults
 import django.core.urlresolvers as urlresolvers
-# from django.core.urlresolvers import RegexURLResolver
-# from django.core.urlresolvers import RegexURLPattern
 
 from .models import CategorySubdomain
-# from ella.core.models import Category, Placement
-# from ella.core.cache import get_cached_object
 
 from ella_category_subdomain.urlresolvers import CategorySubdomainURLPattern
 from ella_category_subdomain.urlresolvers import CategorySubdomainURLResolver
-#from ella_category_subdomain.urlresolvers import CategorySubdomainLocaleURLResolver
 
 
 def get_url_with_subdomain(parsed_url, category_subdomain):
+    # We will need mutable version of parsed_url.
     parsed_url_list = list(parsed_url)
     subdomain_slug = category_subdomain.subdomain_slug
     category_path = category_subdomain.category.tree_path
+    # If parsed_url has scheme attribute then preserve it,
+    # else default to http.
     new_scheme = parsed_url.scheme or 'http'
+    # Strip category tree path (plus slash at the beginning).
     new_path = parsed_url.path[len(category_path)+1:]
+    # Get lowest level domain for given category.
     new_netloc = category_subdomain.category.site.domain
+
+    # If domain starts with 'www.' then replace it with subdomain slug,
     if new_netloc.startswith('www.'):
         new_netloc = new_netloc.replace('www.', subdomain_slug)
-    else:
+    else: # otherwise just prepend subdomain.
         new_netloc = '%s.%s' % (subdomain_slug, new_netloc)
-    if settings.DEBUG and settings.DEVELOPMENT_SERVER_PORT:
+
+    # Append defined server port if debugging.
+    if settings.DEBUG and hasattr(settings, 'DEVELOPMENT_SERVER_PORT'):
         new_netloc += ':%s' % settings.DEVELOPMENT_SERVER_PORT
+
+    # Change parsed_url_list.
     parsed_url_list[:3] = new_scheme, new_netloc, new_path
     logging.warning("parsed_url_list = %r" % parsed_url_list)
+
+    # Construct and return new url.
     return urlunparse(parsed_url_list)
 
 
 def get_url_without_subdomain(parsed_url):
-    return parsed_url
+    parts = parsed_url.netloc.split('.')
+    logging.warning("get_url_without_subdomain.parts = %r" % parts)
+
+    # We will need mutable version of parsed_url.
+    parsed_url_list = list(parsed_url)
+
+    # Get all_sites domains.
+    # FIXME: It desperately requires another approach.
+    all_sites = [x.domain for x in Site.objects.all()]
+
+    for i in range(2, len(parts)):
+        # Construct domains up from the second level
+        domain = '.'.join(parts[-i:])
+        # and if one of them is in all_domains,
+        if domain in all_domains:
+            # pick this domain.
+            parsed_url_list[1] = domain
+            break
+        # FIXME: What if domain of higher level is registered Site?
+
+    # Construct and return new url.
+    return urlunparse(parsed_url_list)
+
 
 def patch_reverse(reverse):
     def wrapper(*args, **kwargs):
+        # Get a result of original Django reverse.
+        # FIXME: We can get MatchResult, no basestring.
         url = reverse(*args, **kwargs)
         logging.warning("Unpatched reverse: %r\n" % url)
+
+        # Parse url.
         parsed_url = urlparse(reverse(*args, **kwargs))
         logging.warning("Parsed unpatched reverse: %r\n" % (parsed_url,))
+
+        # If a path of the original Django reverse starts with tree_path
+        # of a category with subdomain, add the appropriate CategorySubdomain
+        # to category_subdomain_list.
         category_subdomain_list = [x for x in CategorySubdomain.objects.all()
               if parsed_url.path.startswith('/%s/' % x.category.tree_path)]
+        logging.warning("category_subdomain_list = %r\n" % (parsed_url,))
+
+        # If category_subdomain_list is empty, return url without sudomain (of
+        # the lowest possible level).
         if not category_subdomain_list:
             return get_url_without_subdomain(parsed_url)
+
+        # Now, category_subdomain_list should contain just one
+        # CategorySubdomain instance (because of model validation).
+        # Otherwise at least on of them would be double or more nested
+        # category.
         assert len(category_subdomain_list) == 1, 'This should be always 1!'
+
+        # Finally, return url with correct subdomain and path.
         return get_url_with_subdomain(parsed_url, category_subdomain_list[0])
     return wrapper
 
 
-original_url = django.conf.urls.defaults.url
-
-def url(regex, view, kwargs=None, name=None, prefix=''):
-    regex_url = original_url(regex, view, kwargs, name, prefix)
-    if isinstance(regex_url, django.core.urlresolvers.RegexURLPattern):
-        regex_url = CategorySubdomainURLPattern(regex, regex_url)
-    if isinstance(regex_url, django.core.urlresolvers.RegexURLResolver):
-        regex_url = CategorySubdomainURLResolver(regex, regex_url)
-    return regex_url
-
-
 def do_monkeypatch():
+    # Replace django.core.urlresolvers.reverse.
     urlresolvers.reverse = patch_reverse(urlresolvers.reverse)
