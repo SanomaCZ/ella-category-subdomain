9926cdd96e0c0ef53b4a926ade9952172450b105
Jakub Skaryd <skaryd@sanoma.cz>
2011-12-06 13:20:11 +0100

    Middleware split

--- ella_category_subdomain/middleware.py
+++ ella_category_subdomain/middleware.py
@@ -15,19 +15,15 @@ from ella_category_subdomain.monkeypatch import get_url
 from ella_category_subdomain.util import get_domain_for_category
 
 
-class CategorySubdomainMiddleware:
+class CategorySubdomainMiddleware(object):
 
     def __init__(self):
+        super(CategorySubdomainMiddleware, self).__init__()
         self.static_prefixes = [settings.MEDIA_URL,]
 
     def process_request(self, request):
-        redirect = None
-        if ((hasattr(settings, 'CATEGORY_SUBDOMAIN_REDIRECT_OLD')) and
-            (settings.CATEGORY_SUBDOMAIN_REDIRECT_OLD)):
-            redirect = self._get_redirect_if_old_url(request)
-        if redirect is None:
-            self._translate_path(request)
-        return redirect
+        self._translate_path(request)
+        return None
 
     def _get_category_subdomain_for_host(self, host):
         domain_parts = host.split('.')
@@ -39,37 +35,32 @@ class CategorySubdomainMiddleware:
 
             try:
                 category_subdomain = CategorySubdomain.objects.get(subdomain_slug=subdomain)
+
                 if category_subdomain.category.site.pk == settings.SITE_ID:
                     result = category_subdomain
+
             except CategorySubdomain.DoesNotExist:
                 pass
         return result
 
-    def _get_category_subdomain_for_path(self, path):
-        tree_path = path.lstrip('/').split('/')[0]
-        try:
-            return CategorySubdomain.objects.get(category__tree_path=tree_path,
-                                                 category__site__pk=settings.SITE_ID)
-        except CategorySubdomain.DoesNotExist:
-            return None
-
     def _translate_path(self, request):
         '''Change request.path and request.path_info if "necessary".'''
-        # FIXME: What is the best method of proper static prefixes handling?
         for prefix in self.static_prefixes:
-            # FIXME: Should we take request.path or request.path_info?
             if request.path_info.startswith(prefix):
                 return
 
         category_subdomain = self._get_category_subdomain_for_host(request.get_host())
         if category_subdomain is not None:
-            # FIXME: Should we really change both request.path and
-            # request.path_info? "None of them" is not an acceptable answer
-            # here ;-)
             request.path = request.path_info =\
                     '/%s%s' % (category_subdomain.category.path, request.path_info)
             logging.debug("Category subromain %s, new path: %s", category_subdomain, request.path)
 
+
+class CategorySubdomainRedirectMiddleware(object):
+
+    def process_request(self, request):
+        redirect = self._get_redirect_if_old_url(request)
+        return redirect
 
     def _get_redirect_if_old_url(self, request):
         # get the site domain
@@ -96,3 +87,10 @@ class CategorySubdomainMiddleware:
                 # redirect to the new uri
                 return HttpResponseRedirect(urlunparse(parsed_url_list))
 
+    def _get_category_subdomain_for_path(self, path):
+        tree_path = path.lstrip('/').split('/')[0]
+        try:
+            return CategorySubdomain.objects.get(category__tree_path=tree_path,
+                                                 category__site__pk=settings.SITE_ID)
+        except CategorySubdomain.DoesNotExist:
+            return None
