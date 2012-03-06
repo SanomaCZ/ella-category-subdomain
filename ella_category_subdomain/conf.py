c9d617ef43d86b76a0a0b310b1e275c0df11229d
skada <skaryd@skada.org>
2012-03-06 11:53:50 +0100

    Prevent None in IGNORE_PATHS

--- ella_category_subdomain/conf.py
+++ ella_category_subdomain/conf.py
@@ -5,7 +5,10 @@ from ella.utils.settings import Settings
 OLD_STYLE_URL = False
 
 IGNORE_PATHS = ['/feeds/rss', '/feeds/atom', ]
-IGNORE_PATHS.extend([settings.MEDIA_URL, settings.STATIC_URL])
+if (settings.MEDIA_URL):
+    IGNORE_PATHS.append(settings.MEDIA_URL)
+if (settings.STATIC_URL):
+    IGNORE_PATHS.append(settings.STATIC_URL)
 
 ella_category_subdomain_settings = Settings('ella_category_subdomain.conf',
                                             'CATEGORY_SUBDOMAIN')
